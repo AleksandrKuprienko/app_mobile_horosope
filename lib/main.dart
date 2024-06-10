54d53871b1dfdad4aead3e899c67ad22d44be01b
oleksandr.kupriienko <4kuper@gmail.com>
2024-06-11 00:59:17 +0300

    core_firebase setup android/ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:app_mobile_horosope/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -10,6 +15,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: Scaffold(),
+    );
   }
 }
