caefb0b7271fb47fcfdbe79e5856ae8120e71690
oleksandr.kupriienko <4kuper@gmail.com>
2024-09-08 05:43:13 +0300

    add  main navigator

--- lib/navigator/main_navigator.dart
+++ lib/navigator/main_navigator.dart
@@ -1,5 +1,40 @@
+import 'package:app_mobile_horosope/features/home/pages/home_page.dart';
+import 'package:app_mobile_horosope/features/onboarding/pages/onboarding_page.dart';
+import 'package:app_mobile_horosope/features/registration/presentation/pages/registration_page.dart';
+import 'package:app_mobile_horosope/features/user/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class MainNavigator {
   static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  static registration() {
+    navigatorKey.currentState?.pushReplacement(MaterialPageRoute(builder: (context) => const RegistrationPage()));
+  }
+
+  static login() {
+    navigatorKey.currentState?.pushReplacement(MaterialPageRoute(builder: (context) => const LoginPage()));
+  }
+
+  static home() {
+    navigatorKey.currentState?.pushReplacement(PageRouteBuilder(
+      transitionDuration: const Duration(milliseconds: 600),
+      pageBuilder: (context, animation, secondaryAnimation) => const HomePage(),
+      // barrierColor: const Color(0xFF6E56CF),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(0.0, 0.9);
+        const end = Offset.zero;
+        final tween = Tween(begin: begin, end: end);
+        final offsetAnimation = animation.drive(tween);
+        return SlideTransition(
+          position: offsetAnimation,
+          // opacity: animation,
+          child: child,
+        );
+      },
+    ));
+  }
+
+  static onboarding() {
+    navigatorKey.currentState?.pushReplacement(MaterialPageRoute(builder: (context) => const OnboardingPage()));
+  }
 }
--- lib/notifications/app_notifications.dart
+++ lib/notifications/app_notifications.dart
@@ -3,15 +3,35 @@ import 'package:flutter/material.dart';
 class AppNotifications {
   static GlobalKey<ScaffoldMessengerState> notificationsKey = GlobalKey<ScaffoldMessengerState>();
 
-  static successSnackBar() {
+  static successSnackBar(String? message) {
     notificationsKey.currentState!.showSnackBar(
       SnackBar(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         elevation: 0,
         behavior: SnackBarBehavior.floating,
-        backgroundColor: Colors.green[300],
-        showCloseIcon: true,
-        content: const Text(
-          "test",
+        backgroundColor: const Color(0xffF7F8FB),
+        content: Center(
+          child: Text(
+            message ?? 'Success',
+            style: const TextStyle(color: Colors.black),
+          ),
+        ),
+        duration: const Duration(seconds: 2),
+      ),
+    );
+  }
+
+  static errorSnackBar(String error) {
+    notificationsKey.currentState!.showSnackBar(
+      SnackBar(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        elevation: 0,
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: const Color(0xFFB33822),
+        content: Center(
+          child: Text(
+            error,
+          ),
         ),
         duration: const Duration(seconds: 2),
       ),
